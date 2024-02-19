a0841395e4469e3b00be4a643fdf405da6f6661c
Rubén Díaz Dugo <78511737+rubendd@users.noreply.github.com>
2024-02-19 17:21:13 +0100

    terminado

--- addons/realstate/__manifest__.py
+++ addons/realstate/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "Real State",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Módulo creado por Rubén Díaz Dugo""",
 
     'description': """
         Long description of module's purpose
